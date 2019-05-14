6bd38b92a0769217695c12b935bdce6efedd3d73
mattjp <mattjp@umich.edu>
2019-05-14 18:52:34 -0400

    venv

--- src/proj.py
+++ src/proj.py
@@ -2,8 +2,12 @@ class Proj:
   def __init__(self):
     print("Hello World!")
 
+  def func(self):
+    print("func")
+
 def main():
   p = Proj()
+  p.func()
 
 if __name__ == "__main__":
   main()
